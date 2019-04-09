2c5e3b7c2e365dacd2a39c5766517b6764388c42
Jerren Trifan <nerrej01@gmail.com>
2019-04-09 19:51:05 -0400

    Add project files.

---
+++ IdGenerator/IDGeneratorForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace IdGenerator
+{
+    partial class IDGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.IDListLabel = new System.Windows.Forms.Label();
+            this.IDListBox = new System.Windows.Forms.ListBox();
+            this.LatestIDSetupLabel = new System.Windows.Forms.Label();
+            this.LatestIDLabel = new System.Windows.Forms.Label();
+            this.InUseSetupLabel = new System.Windows.Forms.Label();
+            this.InUseLabel = new System.Windows.Forms.Label();
+            this.ExportIDButton = new System.Windows.Forms.Button();
+            this.ClearIDButton = new System.Windows.Forms.Button();
+            this.ImportIDButton = new System.Windows.Forms.Button();
+            this.ClearIDList = new System.Windows.Forms.Button();
+            this.OpenIDFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(12, 18);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(133, 41);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate ID";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // IDListLabel
+            // 
+            this.IDListLabel.AutoSize = true;
+            this.IDListLabel.Location = new System.Drawing.Point(234, 20);
+            this.IDListLabel.Name = "IDListLabel";
+            this.IDListLabel.Size = new System.Drawing.Size(58, 13);
+            this.IDListLabel.TabIndex = 1;
+            this.IDListLabel.Text = "ID\'s in Use";
+            // 
+            // IDListBox
+            // 
+            this.IDListBox.FormattingEnabled = true;
+            this.IDListBox.Location = new System.Drawing.Point(204, 41);
+            this.IDListBox.Name = "IDListBox";
+            this.IDListBox.Size = new System.Drawing.Size(118, 173);
+            this.IDListBox.TabIndex = 2;
+            // 
+            // LatestIDSetupLabel
+            // 
+            this.LatestIDSetupLabel.AutoSize = true;
+            this.LatestIDSetupLabel.Location = new System.Drawing.Point(12, 83);
+            this.LatestIDSetupLabel.Name = "LatestIDSetupLabel";
+            this.LatestIDSetupLabel.Size = new System.Drawing.Size(106, 13);
+            this.LatestIDSetupLabel.TabIndex = 3;
+            this.LatestIDSetupLabel.Text = "Latest ID Generated:";
+            // 
+            // LatestIDLabel
+            // 
+            this.LatestIDLabel.AutoSize = true;
+            this.LatestIDLabel.Location = new System.Drawing.Point(118, 83);
+            this.LatestIDLabel.Name = "LatestIDLabel";
+            this.LatestIDLabel.Size = new System.Drawing.Size(27, 13);
+            this.LatestIDLabel.TabIndex = 4;
+            this.LatestIDLabel.Text = "N/A";
+            // 
+            // InUseSetupLabel
+            // 
+            this.InUseSetupLabel.AutoSize = true;
+            this.InUseSetupLabel.Location = new System.Drawing.Point(49, 108);
+            this.InUseSetupLabel.Name = "InUseSetupLabel";
+            this.InUseSetupLabel.Size = new System.Drawing.Size(69, 13);
+            this.InUseSetupLabel.TabIndex = 5;
+            this.InUseSetupLabel.Text = "Is ID in use?:";
+            // 
+            // InUseLabel
+            // 
+            this.InUseLabel.AutoSize = true;
+            this.InUseLabel.Location = new System.Drawing.Point(118, 108);
+            this.InUseLabel.Name = "InUseLabel";
+            this.InUseLabel.Size = new System.Drawing.Size(27, 13);
+            this.InUseLabel.TabIndex = 6;
+            this.InUseLabel.Text = "N/A";
+            // 
+            // ExportIDButton
+            // 
+            this.ExportIDButton.Location = new System.Drawing.Point(12, 198);
+            this.ExportIDButton.Name = "ExportIDButton";
+            this.ExportIDButton.Size = new System.Drawing.Size(133, 41);
+            this.ExportIDButton.TabIndex = 7;
+            this.ExportIDButton.Text = "Export ID\'s";
+            this.ExportIDButton.UseVisualStyleBackColor = true;
+            this.ExportIDButton.Click += new System.EventHandler(this.ExportIDButton_Click);
+            // 
+            // ClearIDButton
+            // 
+            this.ClearIDButton.Location = new System.Drawing.Point(12, 245);
+            this.ClearIDButton.Name = "ClearIDButton";
+            this.ClearIDButton.Size = new System.Drawing.Size(133, 41);
+            this.ClearIDButton.TabIndex = 8;
+            this.ClearIDButton.Text = "Clear Saved ID\'s";
+            this.ClearIDButton.UseVisualStyleBackColor = true;
+            this.ClearIDButton.Click += new System.EventHandler(this.ClearIDButton_Click);
+            // 
+            // ImportIDButton
+            // 
+            this.ImportIDButton.Location = new System.Drawing.Point(12, 151);
+            this.ImportIDButton.Name = "ImportIDButton";
+            this.ImportIDButton.Size = new System.Drawing.Size(133, 41);
+            this.ImportIDButton.TabIndex = 9;
+            this.ImportIDButton.Text = "Import ID\'s";
+            this.ImportIDButton.UseVisualStyleBackColor = true;
+            this.ImportIDButton.Click += new System.EventHandler(this.ImportIDButton_Click);
+            // 
+            // ClearIDList
+            // 
+            this.ClearIDList.Location = new System.Drawing.Point(214, 223);
+            this.ClearIDList.Name = "ClearIDList";
+            this.ClearIDList.Size = new System.Drawing.Size(97, 27);
+            this.ClearIDList.TabIndex = 10;
+            this.ClearIDList.Text = "Clear ID List";
+            this.ClearIDList.UseVisualStyleBackColor = true;
+            this.ClearIDList.Click += new System.EventHandler(this.ClearIDList_Click);
+            // 
+            // OpenIDFileButton
+            // 
+            this.OpenIDFileButton.Location = new System.Drawing.Point(214, 258);
+            this.OpenIDFileButton.Name = "OpenIDFileButton";
+            this.OpenIDFileButton.Size = new System.Drawing.Size(97, 28);
+            this.OpenIDFileButton.TabIndex = 11;
+            this.OpenIDFileButton.Text = "Open ID File";
+            this.OpenIDFileButton.UseVisualStyleBackColor = true;
+            this.OpenIDFileButton.Click += new System.EventHandler(this.OpenIDFileButton_Click);
+            // 
+            // IDGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.OpenIDFileButton);
+            this.Controls.Add(this.ClearIDList);
+            this.Controls.Add(this.ImportIDButton);
+            this.Controls.Add(this.ClearIDButton);
+            this.Controls.Add(this.ExportIDButton);
+            this.Controls.Add(this.InUseLabel);
+            this.Controls.Add(this.InUseSetupLabel);
+            this.Controls.Add(this.LatestIDLabel);
+            this.Controls.Add(this.LatestIDSetupLabel);
+            this.Controls.Add(this.IDListBox);
+            this.Controls.Add(this.IDListLabel);
+            this.Controls.Add(this.GenerateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "IDGeneratorForm";
+            this.ShowIcon = false;
+            this.Text = "ID Generator";
+            this.Load += new System.EventHandler(this.IDGeneratorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Label IDListLabel;
+        private System.Windows.Forms.ListBox IDListBox;
+        private System.Windows.Forms.Label LatestIDSetupLabel;
+        private System.Windows.Forms.Label LatestIDLabel;
+        private System.Windows.Forms.Label InUseSetupLabel;
+        private System.Windows.Forms.Label InUseLabel;
+        private System.Windows.Forms.Button ExportIDButton;
+        private System.Windows.Forms.Button ClearIDButton;
+        private System.Windows.Forms.Button ImportIDButton;
+        private System.Windows.Forms.Button ClearIDList;
+        private System.Windows.Forms.Button OpenIDFileButton;
+    }
+}
+
---
+++ IdGenerator/IDGeneratorForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Troschuetz.Random;
+
+namespace IdGenerator
+{
+    public partial class IDGeneratorForm : Form
+    {
+    // Variable Initialization
+
+        // Initialize the array that is going to be used for storing IDs on the backend
+        List<int> IDList = new List<int>();
+        // Create the SaveFile path variable which will be used to store and import the list of IDs.
+        string SaveFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\IDList.txt";
+
+    // Class Code
+        // Constructor function for the form
+        public IDGeneratorForm()
+        {
+            // Initialize and declare the GUI Components.
+            InitializeComponent();
+        }
+
+
+      // GUI Functions
+        // This function runs on the start of the program. It clears the internal memory just in case, and also imports the ID's from the save file if there is one.
+        private void IDGeneratorForm_Load(object sender, EventArgs e)
+        {
+            ClearIDListItems();
+            ImportIDsFromFile();
+
+        }
+
+        // Calls AddIDToList() on button press -> ID Generation
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            AddIDToList();
+        }
+        
+        // Calls ImportIDsFromFile() on button press -> Import IDs from File
+        private void ImportIDButton_Click(object sender, EventArgs e)
+        {
+            ImportIDsFromFile();
+        }
+
+        // Calls ExportIDsToFile() on button press -> Export IDs to File
+        private void ExportIDButton_Click(object sender, EventArgs e)
+        {
+            ExportIDsToFile();
+        }
+
+        // Calls ClearSavedIDs() on button press -> Clear IDs from File
+        private void ClearIDButton_Click(object sender, EventArgs e)
+        {
+            ClearSavedIDs();
+        }
+
+        // Calls ClearIDListItems() on button press -> Clear IDs from backend
+        private void ClearIDList_Click(object sender, EventArgs e)
+        {
+            ClearIDListItems();
+        }
+
+        // Calls OpenIDFile on button press -> Opens ID File with Notepad
+        private void OpenIDFileButton_Click(object sender, EventArgs e)
+        {
+            OpenIDFile();
+        }
+
+
+     // Algorithmic / Logical Functions
+        // This function is used to Open the ID File with the default notepad program.
+        private void OpenIDFile()
+        {
+            // Declare Notepad path for opening executable
+            string notepadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "notepad.exe");
+            // Start Notepad process
+            System.Diagnostics.Process.Start(notepadPath, SaveFile);
+        }
+
+        // This function is used to import the saved ID's from the file that is stored on the computer, and also clears the items that are stored in memory.
+        private void ImportIDsFromFile()
+        {
+            // Check if ID FIle already exists
+            if (File.Exists(SaveFile))
+            {
+                // Clear the ID List from memory
+                IDList.Clear();
+                // Clear the visible box from the Form
+                IDListBox.Items.Clear();
+                // Read all lines from the file that saves the IDs
+                var idFile = File.ReadAllLines(SaveFile);
+                // Convert the imported list to a initialized list
+                List<string> _IdList = idFile.ToList();
+                // For each ID in list, do the following 
+                foreach (var id in _IdList)
+                {
+                    // Convert string ID to an Integer
+                    int _id = Convert.ToInt32(id);
+                    // Add converted ID to the backend list
+                    IDList.Add(_id);
+                    // Add converted ID to the frontend list
+                    IDListBox.Items.Add(_id);
+                }
+            }
+        }
+
+        // This function is used to export the ID's from the internal memory to the external save file.
+        private void ExportIDsToFile()
+        {
+            // Create a streamwriter object to be able to write to the ID File stored for backup.
+            using (StreamWriter writeid = new StreamWriter(SaveFile)) // Create streamwriter object with file path
+                foreach (int id in IDList) // For each ID that is in the backend list,
+                    writeid.WriteLine(id); // Write each ID to the end of the file.
+        }
+
+        // This function is used to add the randomly generated ID to internal memory list, and use comparators to verify its integrity.
+        private void AddIDToList()
+        {
+            // Call the algorithmic function to generate a random ID.
+            int ID = GenerateId();
+            // Check if the list already contains the ID that has been made.
+            if (!IDList.Contains(ID)) // If the list doesn't contains the ID
+            {
+                // Add the confirmed ID to the backend list
+                IDList.Add(ID);
+                // Add the confirmed ID to the frontend list
+                IDListBox.Items.Add(ID);
+                // Set the Form Display label to the ID, so that it accurately displays the latest item.
+                LatestIDLabel.Text = ID.ToString();
+                // Set the same label to false, since the ID doesn't match any ones that have been found in the list.
+                InUseLabel.Text = "False";
+            }
+            else // If the list already contains the ID
+                InUseLabel.Text = "True"; // Set the label to true to display that the ID is a duplicate.
+        }
+
+        // This function clears the IDs that are currently being stored in the save file.
+        private void ClearSavedIDs()
+        {
+            // Check if the file already exists before clearing.
+            if (File.Exists(SaveFile))
+                File.WriteAllText(SaveFile, String.Empty); // Replace all the text in the file with an empty string, effectively removing all of its contents.
+        }
+
+        // This function is used to clear the frontend and backend of the ID List.
+        private void ClearIDListItems()
+        {
+            // Clear the list of items in the backend memory.
+            IDList.Clear();
+            // CLear the frontend list of items.
+            IDListBox.Items.Clear();
+        }
+
+        // This function is the generation method that uses a random algorithm to ensure secure and truly random items. This method makes a call to a third party library to ensure a more random number.
+        private int GenerateId()
+        {
+            // Initiate imported library object
+            var _TRandom = new TRandom();
+            // Create a double that uses a random algorithm to get a more random number.
+            double Number = (_TRandom.Next(100000,999999) + _TRandom.Next(100000,999999)) / 2;
+            // Take the random double, which is rounded to consider for odd numbers, and then implement them into a conversion algorithm. 
+            return Convert.ToInt32(Math.Floor(Number)); // Return the int back to the place where it was called from.
+        }
+    }
+}
